273f4d67844467958a9b891120ae1c24d98b7991
veenhof <35380772+veenhof@users.noreply.github.com>
2019-01-10 18:51:53 +0100

    Update and rename Hydr_FULL.ino to Arduino_Hydroponics.ino

--- Arduino_Hydroponics.ino
+++ Arduino_Hydroponics.ino
@@ -3,18 +3,18 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "DHT.h"
+#include <EEPROM.h>
+#include <GravityTDS.h>
 /********************************************************************/
 //Werkende Sensors:
-//1. PH  pin a2
+//1. PH  pin A2
 //2. DHT pin D7       5v
 //3. Waterflow sensor D2    5v
 //4. WaterTemp sensor D3
+//5. TDS Sensor A1
 
-//MOET NOG GEBEUREN:
-//2. TDS op Arduino
-//
-//
-// Data wire is plugged into pin 2 on the Arduino
+// Data wire is plugged into pin 2 on the Arduino??
+
 #define ONE_WIRE_BUS 3        //WaterTemp sensor  D2
 #define SensorPin A0          //pH meter Analog output to Arduino Analog Input 2
 #define DHTPIN 7     // what digital pin we're connected to
@@ -23,31 +23,30 @@
 #define samplingInterval 20
 #define printInterval 800
 #define ArrayLenth  40    //times of collection
+#define TdsSensorPin A1   // TDS Sensor output on A1
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
 int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
 int pHArrayIndex=0;
 OneWire oneWire(3);
 DallasTemperature sensors(&oneWire);
-
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 DHT dht(DHTPIN, DHTTYPE);
-
+GravityTDS gravityTds;
 byte statusLed    = 13;
-
-
 byte sensorInterrupt = 0;  // 0 = digital pin 2
 byte sensorPin       = 2;
 
 // The hall-effect flow sensor outputs approximately 4.5 pulses per second per
 // litre/minute of flow.
 float calibrationFactor = 4.5;
-
 volatile byte pulseCount;  
-
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
-
 unsigned long oldTime;
+
+float temperature = 25,tdsValue = 0;  //Temp for TDS
+
 
 void setup(void) {
   pinMode(LED,OUTPUT);
@@ -71,6 +70,11 @@ void setup(void) {
 
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 
+//TDS
+    gravityTds.setPin(TdsSensorPin);
+    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+    gravityTds.begin();  //initialization
 }
 void loop(void)  {
 
@@ -100,7 +104,17 @@ void loop(void)  {
         printTime=millis();
 
         }
-
+//TDS sensor
+ //temperature = readTemperature();  //add your temperature sensor and read it
+ //value, that is: TDS = EC / 2
+    Serial.println();
+    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+    gravityTds.update();  //sample and calculate 
+    tdsValue = gravityTds.getTdsValue();  // then get the value
+    Serial.print("PPM : ");
+    Serial.print(tdsValue,0);
+    Serial.println(); 
+     
   delay(1000);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
