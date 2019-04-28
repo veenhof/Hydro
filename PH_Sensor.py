e42f5053dd63e549c4d0c873cffeee1ad39cbb2f
veenhof <35380772+veenhof@users.noreply.github.com>
2019-04-28 12:01:17 +0200

    Update PH_Sensor.py

--- PH_Sensor.py
+++ PH_Sensor.py
@@ -1,76 +1,25 @@
 import time
-#import automationhat
-
+from time import sleep
+# import automationhat
 from gpiozero import MCP3008
+
 adc = MCP3008(channel=0)
 adc_ref = 3.3
 voltage = adc_ref * adc.value
-print("channel 0 voltage is: ", voltage)
+f = open('output.txt',"w")
 
+# print("channel 0 voltage is: ", voltage)
 while True:
     try:
         # Read sensor value
         sensor_value = MCP3008(channel=0)
-
         # Calculate PH
         ph = 7 - 1000 * (float)(voltage) * adc_ref / 59.16 / 1023
-
-        print(" ph =", ph)
-    except IOError:
-        print ("Error")
-
-
-
-        
-        
-#hieronder nodig voor automationhat        
-        
-        
-import time
-import automationhat
-
-from gpiozero import MCP3008
-adc = MCP3008(channel=0)
-adc_ref = 3.3
-voltage = adc_ref * adc.value
-print("channel 0 voltage is: ", voltage)
-
-while True:
-    try:
-        # Read sensor value
-        sensor_value = MCP3008(channel=0).read()
-
-        # Calculate PH
-        ph = 7 - 1000 * (float)(sensor_value) * adc_ref / 59.16 / 1023
-
-        print("sensor_value =", sensor_value, " ph =", ph)
-        time.sleep(10)
-    except IOError:
-        print ("Error")
-
-
-###
-#onderstaande is voor automationhat
-
-# Connect the Grove PH Sensor to analog port A0
-# SIG,NC,VCC,GND
-#sensor = automationhat.analog.one.read()
-#print(value)
-
-#time.sleep(1)
-
-# Reference voltage of ADC is 5v
-#adc_ref = 5
-
-#while True:
-#    try:
-        # Read sensor value
- #       sensor_value = automationhat.analog.one.read()
-
-        # Calculate PH
-#        ph = 7 - 1000 * (float)(sensor_value) * adc_ref / 59.16 / 1023
-
-        print("sensor_value =", sensor_value, " ph =", ph)
-        time.sleep(10)
-    except IOError:
-        print ("Error")
+        s = str(ph)
+        f.write(s)
+        sleep(10)
+        break
+    except ValueError:
+        print "Oops!  That was not a valid number. Try again..."
+    finally:
+        print 'all done, bye'
