760afe3645149b7aa73d5e25d74e27f0cec346d0
veenhof <35380772+veenhof@users.noreply.github.com>
2019-07-09 01:40:34 +0200

    Update ph_min.py

--- ph_min.py
+++ ph_min.py
@@ -6,26 +6,6 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-#Some Info
-#GPIO | Relay   |   Function   5V  / 12V Relais
-#---------------|
-#17    1        | 1ste pomp PH -
-#27    2        | 2de  Pomp PH +
-#22    3        | 2x Feeding Pumps
-#25    4        | Waterpomp MAIN
-#24    5        | Waterpomp naar Koelvat
-#23    6        | Fans "In & Uit"
-#NVT   7        |
-#NVT   8        |
-#
-#GPIO | Relay   |   Function  220V RELAIS!
-#---------------|
-#21      1      |
-#20      2      | Cyclops Watermenger
-#16      3      | COB LEDJES
-#NVT     4      |
-
-
 pinList = [17]
 
 # loop through pins and set mode and state to 'low'
@@ -36,15 +16,13 @@ for i in pinList:
 
 # time to sleep between operations in the main loop
 
-SleepTimeL = 2 # 2 sec
+SleepTimeL = 3 # 3 sec
 
 # main loop
 
 try:
   GPIO.output(17, GPIO.LOW)
   time.sleep(SleepTimeL);
-#  GPIO.output(27, GPIO.LOW)
-#  print("TWO")
 #  time.sleep(SleepTimeL);
   GPIO.cleanup()
 
